969be7a3806bb7bd054a9b7a40365b979c4cab34
Juuso Ihatsu <juuso.ihatsu@gmail.com>
2022-03-10 15:52:59 +0200

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {
+  useQuery,
+  gql,
+} from "@apollo/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [dataToggle, setDataToggle] = React.useState(false);
+
+  const GET_CITIES = gql`
+    query GetCities {
+      cities {
+        name,
+        country{name},
+      } 
+    }`;
+
+    const GET_COMPANIES = gql`
+    query GetCompanies {
+      companies {
+        name,
+        websiteUrl,
+      } 
+    }`;
+
+  // function to switch dataToggle's boolean state
+  const handleClick = () => {
+    setDataToggle(!dataToggle);
+  }
+
+  // returns either city or company data depending on dataToggle's state
+  const renderedData = (dataCities, dataCompanies) => {
+    console.log(dataCompanies);
+    if (dataToggle){
+      return (
+        <div>
+          <h2>Cities Data</h2>
+          <table>
+            <tbody>
+              { dataCities.cities.map((city, index) => 
+                <tr key={index}>
+                  <td>{city.name}</td>
+                  <td>{city.country.name}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>Companies Data</h2>
+          <table>
+            <tbody>
+              { dataCompanies.companies.map((company, index) => 
+                <tr key={index}>
+                  <td>{company.name}</td>
+                  <td>{company.websiteUrl}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+  }
+
+  // two queries to gather required data 
+  const { loading: queryCityLoading, error: queryCityError, data: queryCityData } = useQuery(GET_CITIES);
+  const { loading: queryCompLoading, error: queryCompError, data: queryCompData } = useQuery(GET_COMPANIES);
+
+  if (queryCityLoading || queryCompLoading) return <p>Loading....</p>;
+    else if (queryCityError || queryCompError) return <p>Error...</p>
+    else {
+      return (
+        <div className="App">
+          <button onClick={handleClick}>Change dataset</button>
+          <div>{renderedData(queryCityData, queryCompData)}</div>
+        </div>
+      )
+    }
 }
 
 export default App;
